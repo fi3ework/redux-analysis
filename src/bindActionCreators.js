bdc23df8e6731af806a63e1adfbc0d2f785b3c80
Wee <fi3ework@gmail.com>
2018-04-23 16:27:08 +0800

    update

--- src/bindActionCreators.js
+++ src/bindActionCreators.js
@@ -1,4 +1,6 @@
+// 真正需要获取参数的函数被柯里化了起来
 function bindActionCreator (actionCreator, dispatch) {
+  // 高阶函数，闭包引用 dispatch
   return function () {
     return dispatch(actionCreator.apply(this, arguments))
   }
@@ -25,8 +27,13 @@ function bindActionCreator (actionCreator, dispatch) {
  * function as `actionCreators`, the return value will also be a single
  * function.
  */
+
+// 实际上就是返回一个高阶函数，通过闭包引用，将 dispatch 给隐藏了起来
+// 正常操作是发起一个 dispatch(action)，但 bindActionCreators 将 dispatch 隐藏，当执行bindActionCreators返回的函数时，就会dispatch(actionCreators(...arguments))
+// 所以参数叫做 actionCreators，作用是返回一个 action
+// 如果是一个对象里有多个 actionCreators 的话，就会类似 map 函数返回一个对应的对象，每个 key 对应的 value 就是上面所说的被绑定了的函数
 export default function bindActionCreators (actionCreators, dispatch) {
-  // 如果是actionCreators是函数，那么直接调用
+  // 如果是actionCreators是函数，那么直接调用，比如是个需要被thunk的函数
   if (typeof actionCreators === 'function') {
     return bindActionCreator(actionCreators, dispatch)
   }
@@ -46,8 +53,10 @@ export default function bindActionCreators (actionCreators, dispatch) {
     const key = keys[i]
     const actionCreator = actionCreators[key]
     if (typeof actionCreator === 'function') {
+      // 每个 key 再次调用一次 bindActionCreator
       boundActionCreators[key] = bindActionCreator(actionCreator, dispatch)
     }
   }
+  // map 后的对象
   return boundActionCreators
 }
